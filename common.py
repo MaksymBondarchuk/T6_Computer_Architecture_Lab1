be6c7c511a5fd1991c3eea644a1f04feae0b7cf6
Maxim <maksim.bondarchuk@gmail.com>
2015-05-03 20:27:43 +0300

    Gevent

---
+++ common.py
@@ -0,0 +1,74 @@
+import xml.etree.ElementTree as ET
+import re
+import sys
+
+import requests
+
+
+max_depth = 2
+# If you don't want program to print current state
+# to terminal, switch to False
+print_to_terminal = True
+
+
+# Gets data from xml file. Returns urls list
+def get_data_from_xml(xml_name):
+    tree = ET.parse(xml_name)
+    root = tree.getroot()
+
+    urls = []
+    for url in root.findall('url'):
+        urls.append(url.text)
+    return urls
+
+
+# Prints data (emails list) to xml-file
+def print_to_xml(xml_name, emails):
+    root = ET.Element('emails')
+    for email in emails:
+        mail = ET.SubElement(root, 'email')
+        mail.text = email
+    tree = ET.ElementTree(root)
+    tree.write(xml_name, 'utf-8')
+
+
+def get_data_from_url_common(url, depth):
+    # Print to terminal
+    if print_to_terminal:
+        for i in range(2, depth + 1):
+            sys.stdout.write('\t')
+        print("On url: %s, depth: %d" % (url, depth))
+
+    # Trying to download page. If error occurred, returns empty list
+    try:
+        page = requests.get(url)
+    except:
+        return []
+
+    # Getting all emails on this page
+    emails_list = []
+    emails = re.findall('[\w\.-]+@[\w\.-]+', page.text)
+    for email in emails:
+        emails_list.append(email)
+
+        # Print to terminal
+        if print_to_terminal:
+            for i in range(1, depth + 1):
+                sys.stdout.write('\t')
+            print(email)
+
+    # If we cannot go to links on this page
+    if depth == max_depth:
+        return {'emails': emails_list, 'urls': []}
+
+    # Going to all links on this page
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]'
+                      '|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page.text)
+
+    # Removing current url from list
+    try:
+        urls.remove(url)
+    except:
+        pass
+
+    return {'emails': emails_list, 'urls': urls}
--- main.py
+++ main.py
@@ -1,67 +1,20 @@
-import xml.etree.ElementTree as ET
 import time
-import re
-import sys
 
-import requests
+from common import *
 
-
-max_depth = 2
-# If you don't want program to print current state
-# to terminal, switch to False
-print_to_terminal = True
 
 
 # Gets data from url and then calls itself
 def get_data_from_url(url, depth):
-    # Print to terminal
-    if print_to_terminal:
-        for i in range(2, depth + 1):
-            sys.stdout.write('\t')
-        print("On url: %s, depth: %d" % (url, depth))
+    emails_and_urls = get_data_from_url_common(url, depth)  # Dictionary
+    emails_list = emails_and_urls['emails']
+    urls_list = emails_and_urls['urls']
 
-    # Trying to download page. If error occurred, returns empty list
-    try:
-        page = requests.get(url)
-    except:
-        return []
-
-    # Getting all emails on this page
-    emails_list = []
-    emails = re.findall('[\w\.-]+@[\w\.-]+', page.text)
-    for email in emails:
-        emails_list.append(email)
-
-        # Print to terminal
-        if print_to_terminal:
-            for i in range(1, depth + 1):
-                sys.stdout.write('\t')
-            print(email)
-
-    # If we cannot go to links on this page
-    if depth == max_depth:
-        return emails_list
-
-    # Going to all links on this page
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]'
-                      '|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page.text)
-    for _url in urls:
-        if _url != url:
-            emails = get_data_from_url(_url, depth + 1)
-            emails_list.extend(emails)
+    for _url in urls_list:
+        emails = get_data_from_url(_url, depth + 1)
+        emails_list.extend(emails)
 
     return emails_list
-
-
-# Gets data from xml file. Returns urls list
-def get_data_from_xml(xml_name):
-    tree = ET.parse(xml_name)
-    root = tree.getroot()
-
-    urls = []
-    for url in root.findall('url'):
-        urls.append(url.text)
-    return urls
 
 
 # Gets data from urls. Returns emails list
@@ -70,16 +23,6 @@ def get_data_from_urls(urls_list):
     for url in urls_list:
         emails_list.extend(get_data_from_url(url, 1))
     return emails_list
-
-
-# Prints data (emails list) to xml-file
-def print_to_xml(xml_name, emails):
-    root = ET.Element('emails')
-    for email in emails:
-        mail = ET.SubElement(root, 'email')
-        mail.text = email
-    tree = ET.ElementTree(root)
-    tree.write(xml_name, 'utf-8')
 
 
 start_time = time.time()
