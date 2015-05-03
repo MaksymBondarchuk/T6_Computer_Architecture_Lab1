d6db5033d35a53efb4cb24d5ae775fe37913611d
Maxim <maksim.bondarchuk@gmail.com>
2015-05-03 13:26:05 +0300

    First part without comments

--- main.py
+++ main.py
@@ -7,29 +7,44 @@ import requests
 
 
 max_depth = 2
+# If you don't want program to print current state
+# to terminal, switch to False
+print_to_terminal = True
 
+
+# Gets data from url and then calls itself
 def get_data_from_url(url, depth):
-    for i in range(2, depth + 1):
-        sys.stdout.write('\t')
-    print("On url: %s, depth: %d" % (url, depth))
+    # Print to terminal
+    if print_to_terminal:
+        for i in range(2, depth + 1):
+            sys.stdout.write('\t')
+        print("On url: %s, depth: %d" % (url, depth))
 
+    # Trying to download page. If error occurred, returns empty list
     try:
         page = requests.get(url)
     except:
         return []
 
+    # Getting all emails on this page
     emails_list = []
     emails = re.findall('[\w\.-]+@[\w\.-]+', page.text)
     for email in emails:
         emails_list.append(email)
-        for i in range(1, depth + 1):
-            sys.stdout.write('\t')
-        print(email)
 
+        # Print to terminal
+        if print_to_terminal:
+            for i in range(1, depth + 1):
+                sys.stdout.write('\t')
+            print(email)
+
+    # If we cannot go to links on this page
     if depth == max_depth:
         return emails_list
 
-    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page.text)
+    # Going to all links on this page
+    urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]'
+                      '|(?:%[0-9a-fA-F][0-9a-fA-F]))+', page.text)
     for _url in urls:
         if _url != url:
             emails = get_data_from_url(_url, depth + 1)
@@ -38,6 +53,7 @@ def get_data_from_url(url, depth):
     return emails_list
 
 
+# Gets data from xml file. Returns urls list
 def get_data_from_xml(xml_name):
     tree = ET.parse(xml_name)
     root = tree.getroot()
@@ -48,6 +64,7 @@ def get_data_from_xml(xml_name):
     return urls
 
 
+# Gets data from urls. Returns emails list
 def get_data_from_urls(urls_list):
     emails_list = []
     for url in urls_list:
@@ -55,6 +72,7 @@ def get_data_from_urls(urls_list):
     return emails_list
 
 
+# Prints data (emails list) to xml-file
 def print_to_xml(xml_name, emails):
     root = ET.Element('emails')
     for email in emails:
