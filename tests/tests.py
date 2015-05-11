03c4a40ba578e07f5138dda861691b0ccd397e46
Max <maksim.bondarchuk@gmail.com>
2015-05-11 22:36:35 +0300

    With tests

--- tests/tests.py
+++ tests/tests.py
@@ -26,11 +26,30 @@ class Test(unittest.TestCase):
         f = open('html_page.html', 'r')
         html_code = f.read()
 
-        mock.return_value = html_code
+        class HTML:
+            text = html_code
+
+        pseudo_html_page = HTML()
+        pseudo_html_page.text = html_code
+
+        mock.return_value = pseudo_html_page
         request_function.return_value = mock.return_value
 
         res = common.get_data_from_url_common('http://www.cvk.gov.ua/pls/vnd2014/wp312?PT001F01=910', 1)
-        self.assertEqual(res, ['post@cvk.gov.ua'])
+        self.assertEqual(res['emails'], ['post@cvk.gov.ua'])
+
+    # @patch('requests.get')
+    # def test_get_data_from_url(self, request_function):
+    #     mock = Mock()
+    #
+    #     f = open('html_page.html', 'r')
+    #     html_code = f.read()
+    #
+    #     mock.return_value = html_code
+    #     request_function.return_value = mock.return_value
+    #
+    #     res = main.get_data_from_url('http://www.cvk.gov.ua/pls/vnd2014/wp312?PT001F01=910', 1)
+    #     self.assertEqual(res, ['post@cvk.gov.ua'])
 
 
 if __name__ == '__main__':
